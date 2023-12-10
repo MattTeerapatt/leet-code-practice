f0439199ee0031e4c295ac2be27daca1149e4f74
MattTeerapatt <mmatt.trp@gmail.com>
2023-12-10 18:52:23 +0700

    removeElement

---
+++ leet-code-practice/easy/removeElement.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left = 0
+        for right in range(len(nums)):
+            print("time -------", right)
+            if nums[right] != val:
+                nums[left] = nums[right]
+                left += 1
+            print("left = ",left)
+            print("right = ",right, " \n")
+            print(nums," \n")
+        return left
+            
+    
+solution = Solution()
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+result = solution.removeElement(nums, val)
+print(f"Result: {result}, nums = {nums[:result]}")# [start:end:step]
+         
